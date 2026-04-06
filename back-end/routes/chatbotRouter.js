9bd8c6f91f4c4b7083a31635b93011c6e6380a82
Basudeb Bej <basudebbej2025@gmail.com>
2026-04-06 21:56:23 +0530

    Delete back-end directory

--- back-end/routes/chatbotRouter.js
+++
@@ -1,8 +0,0 @@
-// routes/chat.js
-const express = require("express");
-const { handleChat } = require("../controllers/chatbotController");
-
-const router = express.Router();
-router.post("/", handleChat);
-
-module.exports = router;
